858f0dfb0d3542c750b66749fd2a4d4d8d3bcd39
Dan Gowans <dangowans@users.noreply.github.com>
2024-11-21 11:12:57 -0500

    change boolean checks

--- test/test.ts
+++ test/test.ts
@@ -6,17 +6,16 @@ import hasPackage from '../index.js'
 await describe('has-package', async () => {
   await it('Returns `true` for an existing package', async () => {
     const exists = await hasPackage('eslint-config-cityssm')
-    assert(exists === true)
+    assert.strictEqual(exists, true)
   })
-  
+
   await it('Returns `true` for an existing package with organization', async () => {
     const exists = await hasPackage('@types/node')
-    assert(exists === true)
+    assert.strictEqual(exists, true)
   })
 
   await it('Returns `false` for an unavailable package', async () => {
     const exists = await hasPackage('@cityssm/unavailable-package')
-    assert(exists === false)
+    assert.strictEqual(exists, false)
   })
-  
 })
